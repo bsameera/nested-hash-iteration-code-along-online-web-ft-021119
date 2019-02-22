7766977b1b1696ce4b51a727ea595fa6764a2ab3
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-22 04:29:11 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, person_details_hash|
+    person_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        #binding.pry
+        data.delete_if {|ice_cream| ice_cream=="strawberry"}
+      end 
+    end 
+  end 
 end
